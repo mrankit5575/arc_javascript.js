36319a420c499037d04278d5df7509f7b4f02a8d
mrankit5575 <mrankit5575@gmail.com>
2026-07-01 11:29:39 +0530

    udpat the coe

---
+++ arc-notes/script.js
@@ -0,0 +1,91 @@
+// variable  container hota hia jo data store krta hai 
+
+// three types of variable 
+// const 
+// let 
+// var
+
+
+
+
+const name = "ankit singh" 
+// name = "dev"  const kabhi change nhi hota 
+// console.log(name);
+
+let  x = 50 ; 
+x= 4000
+// console.log(x);
+
+x = 5000 ;
+// console.log(x)
+
+// let ki value change ho skti hai but isme condition hoti hai 
+
+
+
+// var old tarika hai jise use krte the 
+// then fir 2015 mai let aya toh var use nhi krte ab 
+ 
+ var y = 'var is old version'
+//  console.log(y);
+  
+
+//  var aur let dono mai difference hai aage 
+
+
+// scope kya hota hai 
+
+// { open 
+    // iske andar code hai scope 
+// } close 
+
+
+// data types kya hote hai  
+
+
+// Primitive types and 
+//  1 Non-Primitive type (Object)
+
+
+//Primitive types  7 types 
+
+// String  kya hota hai 
+// "" iske andar jo bhi likha ho string hoga
+// ex
+// "dev " string hai 
+// "156461268" ye bhi string hai 
+
+// numbers 
+// ex
+// let x = 50.145 ; 
+
+
+// boolean
+// true 
+// false
+
+// // example 
+// if(0===0){
+// console.log("hello")
+// }
+//  if(0===1){
+//     console.log("nhi hai")
+//  }
+
+
+//four data  null data types 
+
+
+//five data types  Undefined
+let d; 
+console.log(d);
+
+// six data types 
+
+// Symbol 
+// example ; 
+// % < # < 8  , @ < ! 
+
+
+// Non-Primitive type
+// object 
