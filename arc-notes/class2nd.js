a8e11e4dd0361f56ff5ee6c938f6d49c68ab4ee0
MRANKIT <mrankit5575@gmail.com>
2026-07-08 22:28:50 +0530

    Create class2nd.js with JS fundamentals examples
    Add JavaScript code examples demonstrating variable declarations, scope, conditionals, and array operations.

---
+++ arc-notes/class2nd.js
@@ -0,0 +1,117 @@
+variable 
+let var const 
+var old method 
+let new method 
+
+// const - constant  fixed value 
+
+
+// scope and global scope 
+
+// global scope 
+// let x = 'arc'
+// console.log(x);
+// //  
+// {
+    // let  x = 'ankit'
+//     console.log(x);
+    
+// }
+ 
+// if else condition 
+
+ let x = 2 
+ let y = "2"
+//  console.log(x==y);
+//  == ye srif check krta hai value 
+// === ye check krta hai value + datatypes 
+//  console.log(x===y);
+
+
+if ('2'===2) {
+    console.log('print');
+    
+    
+}else{
+    console.log('2 is not equal datatypes ');
+    
+}
+
+if ("2"===2) {
+    console.log('2 is equal');
+    
+    
+} else if(3===4) {
+        console.log('equal number');
+
+    
+}else if(5==5){
+    console.log('5 is equal');
+    
+
+}
+
+
+// else{
+// console.log('something print');
+
+// }
+
+
+Question 
+
+console.log(2+2) output - 4
+console.log("2"+2); output - 22
+console.log('2'+'2' + 2); output - 222
+console.log(2 +2 + '2'); output - 42
+console.log('2'+2+2); output -222
+
+
+
+
+// question 
+let name = "raj"
+let secondName = name ;
+console.log(secondName);
+secondName = 'lavish'
+console.log(secondName);
+
+
+// console.log(m); 
+let m = 20 ;
+// tdz zone 
+
+
+// array  ki zarurat 
+let mm = 20 
+let mx = 20 
+let mn = 20 
+let method = 20 
+let mp = 20 
+console.table([mm , mx , mn , method, mp]);
+
+
+// The Array object, as with arrays in other programming languages,
+//  enables storing a collection of multiple items under a single variable 
+//  name,
+//  and has members for performing common array operations
+
+const myArr = ['ankit' , 'lavish', 'dev' , true , false , 57587 ]
+// console.log(myArr);
+// indexing start from 0 
+// console.log(myArr[0]);
+// console.log(myArr[2]);
+
+// length check 
+// console.log(myArr.length);
+
+
+// myArr.push("dhiraj") add last element
+// myArr.push("jkldgj")
+// console.log(myArr);
+
+// myArr.pop() remove the last element
+// console.log(myArr);
+
+
+
