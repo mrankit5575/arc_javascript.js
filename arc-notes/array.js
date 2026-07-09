5f0ae46ff94587848f366a3789dd7207dcd2310d
MRANKIT <mrankit5575@gmail.com>
2026-07-09 11:50:59 +0530

    Document variable scope and array usage in array.js
    Added comments explaining variable scope, arrays, and the spread operator.

---
+++ arc-notes/array.js
@@ -0,0 +1,47 @@
+ //  global scope same name variable nhi use kr skte hai 
+//  error ayega 
+//  SyntaxError: Identifier 'x' has already been declared
+ let x = 61+ 4 + "Volvo";
+//  console.log(x);
+// let x = 'ankit' ye galat hai
+// block scope
+{
+    let x = 100;
+    // console.log(x);
+    
+}
+
+// question
+let xm ; 
+xm = 'ankit'
+// console.log(xm);
+
+
+// array 
+// store multiple value in single variable
+
+const myArr = ['ankit' , 'dhiraj' , 'renu' , 'adi', 252 , true , false ]
+
+
+//  console.log(myArr);
+// indxing base priting
+// console.log(myArr[3]);
+// console.log(myArr[2]);
+
+// dhiraj question 
+const arr = ['dhiraj' , ['adi' , 'renu'] , 'ankit']
+
+//  console.log(arr);
+// console.log(arr[1]);
+// console.log(arr[1][1]);
+
+ const my = ['ankit' ,['renu' ] , ['adi' , 'dhiraj']]
+
+
+ console.log(my.flat(2));
+ 
+//  console.log(my);
+// spread operator 
+// ...
+ 
+ 
